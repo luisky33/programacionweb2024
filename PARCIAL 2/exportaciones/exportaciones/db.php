26ed8841c35497c8dedfc77a4a1321ab2eb616ab
luisky33 <luisantoniom490@gmail.com>
2024-10-28 10:02:58 -0600

    Add files via upload
    1 modificacion (exportaciones):10 avatar.
    2 modificacion(dragon ball modificaciones):estilo propio y añadirle funciones.

---
+++ PARCIAL 2/exportaciones/exportaciones/db.php
@@ -0,0 +1,16 @@
+<?php
+$host = 'localhost';
+$db = 'torneo_db';
+$user = 'root';
+$password = ' ';
+
+try {
+    $pdo =new PDO("mysql:host=$host;dbname=$db",$user,$password);
+    $pdo->setAttribute(PDO:ATTR_ERRMODE,PDO:: ERRMODE_EXCEPTION);
+
+}
+catch (PDOException $e)
+{
+    die("error de conexion : ". $e->getMessage());
+}
+?>
---
+++ PARCIAL 2/exportaciones/exportaciones/js/javascript.js
@@ -0,0 +1,4 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    // Clear all form fields
+    document.getElementById('myForm').reset();
+});
